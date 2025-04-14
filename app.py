ead3343d1ae3e7e66701a9e39b5f70527de45334
nav-neit <rgnavaneeth@gmail.com>
2025-04-14 17:00:22 +0530

    Added Session State variables

--- app.py
+++ app.py
@@ -76,3 +76,42 @@ proficiency_level = st.sidebar.selectbox('Proficiency Level 🏆', PROFICIENCY_L
 
 ## here time_delay is used for specifying the waiting time btw displaying two consecutive messages
 ## beneficial for user to allow enough time to read the generatred messages before the next exhange appears
+
+## streamlit session state to store user-specific session data in the streamlit app
+if "bot1_mesg" not in st.session_state:
+    st.session_state["bot1_mesg"] = []
+    ## this is a list whoseelements are a dictionary that holds the message spoekn by first chatbot
+    ## keys - role, content, translation
+
+if "bot2_mesg" not in st.session_state:
+    st.session_state["bot2_mesg"] = []
+    ## same as bot1_msg
+
+if 'batch_flag' not in st.session_state:
+    st.session_state["batch_flag"] = False
+    ## indicates whether the conversation messages are shown all at once or with a time delay
+    ## the chat between the bots will appear with a time delay when thier conversations are generated for the first time
+    ## when user wants to see the translation/add audio for the generated conv, the stored conversation messages "bot1_mesg" "bot2_mesg" can be shown at once
+    ## this is benefecial since we dont need to call api again , reduces cost and latency
+
+
+if 'translate_flag' not in st.session_state:
+    st.session_state['translate_flag'] = False
+    ## indicates if its a translation
+
+if 'audio_flag' not in st.session_state:
+    st.session_state["audio_flag"] = False
+    ## indicates if its a audio
+
+if 'mmessage_counter' not in st.session_state:
+    st.session_state["message_counter"] = 0
+    ## adds one whenever a message from chatbot is displayed
+    ## also to add message ID with this counter as streamlit requires that each UI components to have a unique ID                      
+
+## streamlit reruns / reload the scripts on every user interaction
+## regular python variables would lose thier values, and the app would reset to its initial state
+## "session_state" in streamlit provides a way to store and retrieve data that persists throughout the user's session
+## even if the app is reloaded or the user navigates between different components or pages
+
+##Letting chatbots interact and generate conversations
+
